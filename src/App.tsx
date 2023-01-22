8d4c3ea96a065f8f16e2c34bccedd733e9204f3e
Joshua-Castillo <joshcastillo2048@gmail.com>
2023-01-22 02:41:57 -0500

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,61 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+// import { PowerBIEmbed } from "powerbi-client-react";
+// import { models } from "powerbi-client";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <h1>Hello Tony Stark</h1>
+      {/* <PowerBIEmbed
+        embedConfig={{
+          type: "report", // Supported types: report, dashboard, tile, visual and qna
+          id: "<Report Id>",
+          embedUrl: "<Embed Url>",
+          accessToken: "<Access Token>",
+          tokenType: models.TokenType.Embed,
+          settings: {
+            panes: {
+              filters: {
+                expanded: false,
+                visible: false,
+              },
+            },
+            background: models.BackgroundType.Transparent,
+          },
+        }}
+        eventHandlers={
+          new Map([
+            [
+              "loaded",
+              function () {
+                console.log("Report loaded");
+              },
+            ],
+            [
+              "rendered",
+              function () {
+                console.log("Report rendered");
+              },
+            ],
+            [
+              "error",
+              function (event) {
+                console.log(event.detail);
+              },
+            ],
+          ])
+        }
+        cssClassName={"report-style-class"}
+        getEmbeddedComponent={(embeddedReport) => {
+          this.report = embeddedReport as Report;
+        }} 
+      />*/}
+    </div>
+  );
+}
+
+export default App;
